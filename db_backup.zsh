01f15754d16e28cde547cdd0d341c7d892f357c2
jr0dd <jree2kx@gmail.com>
2021-03-04 08:27:09 -0500

    tweaked date format and removed tar verbose flag left from testing

--- db_backup.zsh
+++ db_backup.zsh
@@ -2,11 +2,10 @@
 
 # Make a backup of the TrueNAS database and secrets file in tar format that can be uploaded through the gui
 
-# Change the BAKDIR to whatever backup directory you are using
-BAKDIR="/mnt/deadpool/backups/"
+BAKDIR="/mnt/deadpool/backups"
 DATADIR="/data"
 VERSION=$(cut -d ' ' -f1 /etc/version)
-DATE=$(date +%Y%m%d)
+DATE=$(date +%Y%m%d%H%M%S)
 
-tar -C $DATADIR -cvf "$BAKDIR"/"$VERSION"-"$DATE".tar pwenc_secret freenas-v1.db
+tar -C $DATADIR -cf "$BAKDIR"/"$VERSION"-"$DATE".tar pwenc_secret freenas-v1.db
 find "$BAKDIR" -type f -mtime +60d -name "*.tar" -exec rm {} \;
