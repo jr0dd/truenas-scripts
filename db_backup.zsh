94537942f55e79b858968adb23d43a5c675bd593
jr0dd <jree2kx@gmail.com>
2021-03-03 23:37:04 -0500

    use tar for backup
    I updated this to use tar for the backup of the db and the secrets file. I wanted this to be able to be restored from the gui for simplicity if the boot device[s] had a catastrophic failure.

--- db_backup.zsh
+++ db_backup.zsh
@@ -1,10 +1,12 @@
 #!/usr/bin/env zsh
 
+# Make a backup of the TrueNAS database and secrets file in tar format that can be uploaded through the gui
 
-BAK_DIR="PUT FULL BACKUP PATH HERE"
-DB="/data/freenas-v1.db"
-VERSION=$(cat /etc/version | cut -d ' ' -f1)
-DATE=$(date +%Y-%m-%d)
-BAK_FILE="$BAK_DIR"/"$DATE"-"$VERSION".db
-cp "$DB" "$BAK_FILE"
-find "$BAK_DIR" -type f -mtime +60d -name "*.db" -exec rm {} \;
+# Change the BAKDIR to whatever backup directory you are using
+BAKDIR="/mnt/deadpool/backups/"
+DATADIR="/data"
+VERSION=$(cut -d ' ' -f1 /etc/version)
+DATE=$(date +%Y%m%d)
+
+tar -C $DATADIR -cvf "$BAKDIR"/"$VERSION"-"$DATE".tar pwenc_secret freenas-v1.db
+find "$BAKDIR" -type f -mtime +60d -name "*.tar" -exec rm {} \;
