89f16c1d3aee6b85bac56cbdff72a3e4aa9524c8
jr0dd <jree2kx@gmail.com>
2021-03-13 15:11:41 -0500

    replaced echo with printf

--- db_check.zsh
+++ db_check.zsh
@@ -9,19 +9,19 @@ EMAIL=$(awk '{ if ($1 == "root\:") {print $2 } }' /etc/aliases)
 HOSTNAME=$(hostname)
 
 if [[ -f "$DB" && "$DBCHECK" == "ok" ]]; then
-    echo "Database Check OK"
+    printf "Database Check OK"
   else
     if [[ ! -f "$DB" ]]; then
-        echo "Database not present. Please check path."
+        printf "Database not present. Please check path."
       else
         if [[ "$EMAIL" == "None" ]]; then
-            echo "Database error was found, but no email is configured for root user"
-            echo "Outputting error to shell only"
-            echo "ERROR: '$DBCHECK'"
+            printf "%s\n" "Database error was found, but no email is configured for root user"
+            printf "%s\n" "Outputting error to shell only"
+            printf "%s\n" "ERROR: '$DBCHECK'"
           else
             {
             printf "To: %s\n" "$EMAIL"
-            printf "Subject: ERROR: Database corrupt on server %s\n\n" "$HOSTNAME"
+            printf "Subject: TrueNAS %s: Alert: DB Check on %s\n" "$HOSTNAME" "$HOSTNAME"
             printf "%s has a corrupt TrueNAS config. Please restore from a backup ASAP!\n\n\nErrors:\n\n" "$HOSTNAME"
             printf "%s" "$DBCHECK"
             } >> "$TMPFILE"
